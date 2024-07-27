6114f6b8a274833264e61e8d9c003b9a58bad2ec
Ali Budak <yerelcibir@gmail.com>
2024-07-27 10:20:55 +0300

    Dinamik route belirlendi

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name="index"),
+    #path("pyhton", views.pyhton_course, name="pyhton"),
+    #path("java", views.java_course, name="java")
+    path("<str:item>", views.course, name="course"),
+    path("<int:num1>/<int:num2>", views.multiply_view, name="multi"),
 ]
 
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+course_dictionary = {
+     "pyhton" : "Pyhton Course Page",
+     "java" : "Java Course Page",
+     "kotlin" : "Kotlin Course Page",
+     "swift" : "Swift Course Page"
+}
+
+"""
+# Bu da doğru
+def course(request, item):
+     return HttpResponse(course_dictionary[item])
+"""
+
+# Burada get metoduyla alınan item'a default bir değer atıyoruz. "Not Found"
+def course(request, item):
+     return HttpResponse(course_dictionary.get(item, "Not Found"))
+
 def index(request):
     return HttpResponse("Bu benim ilk django mesajım")
 
+"""
+def pyhton_course(request):
+    return HttpResponse("Pyhton Course Page")
+
+def java_course(request):
+        return HttpResponse("Java Course Page")
+"""
+def multiply_view(request, num1, num2):
+     return HttpResponse(f"{num1}*{num2}={num1*num2}")
